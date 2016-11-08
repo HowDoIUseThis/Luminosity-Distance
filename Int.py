c922c8d1b80dfc86807cb4abc7956715574183bb
Christopher Stewart <christopherprattstewart@gmail.com>
2016-11-07 20:09:29 -0500

    I broke some stuff and added some stuff
    Right now im stuck at dealing with a small error that I will debugg when
    im awake.

--- Int.py
+++ Int.py
@@ -4,7 +4,10 @@ This is intro program used to show that I can do computer stuff
 
 Christopher Stewart 11/04/16
 """
+import scipy.integrate as integrate
+import matplotlib.pyplot as plt
 import math
+import numpy as np
 """
 Test values and constants. Below is a list of things to remember:
 
@@ -46,6 +49,8 @@ def linespace(start, end, num):
     diff = (float(end) - start) / (num - 1)
     return [diff * i + start for i in range(num)] #returns the array of points
 
+
+
 def eCalc(type,z,O_m,O_k,O_L):
     z3 = (1+z)**3
     z2 = (1+z)**2
@@ -56,28 +61,65 @@ def eCalc(type,z,O_m,O_k,O_L):
         E = z1*math.sqrt(O_m*z3 +O_k*z2 + O_L)
     return 1/E
 
-
-def simpleInt(type,lowerBound,upperbound,accuracy):
-    tempSpace = linespace(lowerBound,upperbound,accuracy)
+"Ok so this is pretty awful, but this is why we use things smart people make, numpy!"
+def simpleInt(type,lowerBound,upperBound,accuracy):
+    tempSpace = linespace(lowerBound,upperBound,accuracy)
     sum = 0
     for x in tempSpace:
         sum += eCalc(type,x,Omega_M,Omega_K,Omega_L)
-    return sum*(upperbound-lowerBound)/accuracy
+    return sum*(upperBound-lowerBound)/accuracy
 
-
-
+def accurateInt(type,lowerBound,upperBound):
+    result = integrate.quad(lambda x: eCalc(type,x,Omega_M,Omega_K,Omega_L), lowerBound, upperBound)
+    return result
+				
+def printOldInt():
+    z0=simpleInt(1,0,0.5,500)
+    z1=simpleInt(1,0,1,500)
+    z6=simpleInt(1,0,6,500)
+    print("The comoving distances are:","\nZ = 0.5: ",z0, "\nZ = 1: ",z1, "\nZ = 6: ",z6)
+    lz0=(1.5*z0)
+    lz1=(2*z1)
+    lz6=(7*z6)
+    print("When OmegaK is 0 the Luminosity distances are:","\nZ = 0.5: ",
+										lz0, "\nZ = 1: ",lz1, "\nZ = 6: ",lz6)
+    lb0 = simpleInt(0,0,0.5,500)
+    lb1 = simpleInt(0,0,1,500)
+    lb6 = simpleInt(0,0,6,500)
+    print("The lookback timess are:","\nZ = 0.5: ",lb0, "\nZ = 1: ",lb1, "\nZ = 6: ",lb6)
+    
+def printAccInt():
+    print("The comoving distances are:", "\nZ = 0.5: ", accurateInt(1,0,0.5)[0], "\nZ = 1: ", 
+        accurateInt(1, 0, 1)[0], "\nZ = 6: ", accurateInt(1, 0, 6)[0])
+   
+    print("When OmegaK is 0 the Luminosity distances are:","\nZ = 0.5: ",1.5*(accurateInt(1,0,0.5)[0]), 
+				"\nZ = 1: ", 2*(accurateInt(1, 0, 1)[0]), "\nZ = 6: ",7*(accurateInt(1, 0, 6)[0]))
+   
+    print("The lookback timess are:","\nZ = 0.5: ",accurateInt(0,0,0.5)[0],
+					"\nZ = 1: ",accurateInt(0, 0, 1)[0], "\nZ = 6: ",accurateInt(0, 0, 6)[0])
+    
+def convToScale(i):
+	return i[0]
 """
 For testing results
 """
-z0=simpleInt(1,0,0.5,500)
-z1=simpleInt(1,0,1,500)
-z6=simpleInt(1,0,6,500)
-print("The comoving distances are:","\nZ = 0.5: ",z0, "\nZ = 1: ",z1, "\nZ = 6: ",z6)
-lz0=(1.5*z0)
-lz1=(2*z1)
-lz6=(7*z6)
-print("When OmegaK is 0 the Luminosity distances are:","\nZ = 0.5: ",lz0, "\nZ = 1: ",lz1, "\nZ = 6: ",lz6)
-lb0 = simpleInt(0,0,0.5,500)
-lb1 = simpleInt(0,0,1,500)
-lb6 = simpleInt(0,0,6,500)
-print("The lookback timess are:","\nZ = 0.5: ",lb0, "\nZ = 1: ",lb1, "\nZ = 6: ",lb6)
+printAccInt()
+"""
+not working atm
+d1 = 1.5*accurateInt(1,0,0.5)[0]
+d2 = 2*(accurateInt(1, 0, 1))[0]
+d3 = 7*(accurateInt(1, 0, 6))[0]
+x = np.array([d1,d2,d3])
+y = np.linspace(0, 6, num=100)
+x.shape()
+y.shape()
+plt.plot(x,y)
+
+plt.xlabel('Red Shift (z)')
+plt.ylabel('Luminosity Distance (Dl)')
+plt.title('Luminosity Distance vs redshift')
+plt.grid(True)
+plt.savefig("LD_vs_Rs.png")
+plt.show()
+
+"""
